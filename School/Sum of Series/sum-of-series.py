c28e775d630104b8f16e8367bfda6bcf63ff3c48
Devendra Ingle <101554961+DevendraIngle@users.noreply.github.com>
2024-06-08 10:51:05 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum of Series/sum-of-series.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def seriesSum(self, n : int) -> int:
+        # code here
+        result = int((n*(n+1)//2))
+        return result
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.seriesSum(n)
+
+        print(res)
+
+# } Driver Code Ends
